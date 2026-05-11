2962601fa54ee60a0ab1864456b80f6d5132d0e6
tychoid <28597551+tychoid@users.noreply.github.com>
2026-05-11 11:25:21 -0600

    Update libyuv.rb

--- Formula/libyuv.rb
+++ Formula/libyuv.rb
@@ -8,7 +8,10 @@ class Libyuv < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build",
+      "-DCMAKE_CXX_FLAGS=-DLIBYUV_DISABLE_SVE -DLIBYUV_DISABLE_SME",
+      "-DCMAKE_C_FLAGS=-DLIBYUV_DISABLE_SVE -DLIBYUV_DISABLE_SME",
+      *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
