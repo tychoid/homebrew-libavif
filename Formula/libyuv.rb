d4c8f58def6adb20f98f59eeb72e4776e7cb8aee
tychoid <28597551+tychoid@users.noreply.github.com>
2026-05-11 12:12:18 -0600

    Update libyuv.rb

--- Formula/libyuv.rb
+++ Formula/libyuv.rb
@@ -2,19 +2,14 @@ class Libyuv < Formula
   desc "Library for YUV scaling and conversion"
   homepage "https://chromium.googlesource.com/libyuv/libyuv"
   url "https://chromium.googlesource.com/libyuv/libyuv.git",
-      revision: "stable"
+      branch: "stable"
   version "stable"
 
   depends_on "cmake" => :build
 
   def install
-    ENV.append "CXXFLAGS", "-mno-i8mm"
-    ENV.append "CFLAGS", "-mno-i8mm"
-
     system "cmake", "-S", ".", "-B", "build",
-      "-DCMAKE_CXX_FLAGS=-DLIBYUV_DISABLE_SVE -DLIBYUV_DISABLE_SME",
-      "-DCMAKE_C_FLAGS=-DLIBYUV_DISABLE_SVE -DLIBYUV_DISABLE_SME",
-      "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+      "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
       *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
