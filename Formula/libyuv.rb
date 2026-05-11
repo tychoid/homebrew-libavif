1dca5348c690d18728d6c20b45de9096f8518de0
tychoid <28597551+tychoid@users.noreply.github.com>
2026-05-11 11:12:06 -0600

    Create libyuv.rb

---
+++ Formula/libyuv.rb
@@ -0,0 +1,15 @@
+class Libyuv < Formula
+  desc "Library for YUV scaling and conversion"
+  homepage "https://chromium.googlesource.com/libyuv/libyuv"
+  url "https://chromium.googlesource.com/libyuv/libyuv.git",
+      revision: "4b4e68b3"
+  version "4b4e68b3"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+end
