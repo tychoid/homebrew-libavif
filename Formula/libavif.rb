a5b15232e84765abe3ede3dfcbc8b6759a94aaaa
tychoid <28597551+tychoid@users.noreply.github.com>
2026-05-11 10:44:00 -0600

    Update libavif.rb

--- Formula/libavif.rb
+++ Formula/libavif.rb
@@ -2,7 +2,7 @@ class Libavif < Formula
   desc "Library for encoding and decoding .avif files"
   homepage "https://github.com/AOMediaCodec/libavif"
   url "https://github.com/AOMediaCodec/libavif/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "" # fill in after downloading
+  sha256 "914662e16245e062ed73f90112fbb4548241300843a7772d8d441bb6859de45b"
 
   depends_on "cmake" => :build
   depends_on "aom"
@@ -11,10 +11,16 @@ class Libavif < Formula
 
   def install
     args = %W[
-      -DAVIF_CODEC_AOM=ON
-      -DAVIF_CODEC_SVT=ON
+      -DAVIF_CODEC_AOM=LOCAL
+      -DAVIF_CODEC_DAV1D=LOCAL
+      -DAVIF_CODEC_LIBGAV1=LOCAL
+      -DAVIF_CODEC_RAV1E=LOCAL
+      -DAVIF_CODEC_SVT=LOCAL
+      -DAVIF_JPEG=LOCAL
+      -DAVIF_LIBYUV=LOCAL
+      -DAVIF_ZLIBPNG=LOCAL
       -DAVIF_BUILD_APPS=ON
-      -DAVIF_LIBYUV=OFF
+      
     ]
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
