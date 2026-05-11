ed36084033f92b0092412d6cfa32293fb7189969
tychoid <28597551+tychoid@users.noreply.github.com>
2026-05-11 12:01:39 -0600

    Update libyuv.rb

--- Formula/libyuv.rb
+++ Formula/libyuv.rb
@@ -2,13 +2,15 @@ class Libyuv < Formula
   desc "Library for YUV scaling and conversion"
   homepage "https://chromium.googlesource.com/libyuv/libyuv"
   url "https://chromium.googlesource.com/libyuv/libyuv.git",
-      revision: "ace7c457"
-  version "ace7c457"
+      revision: "stable"
+  version "stable"
 
   depends_on "cmake" => :build
 
   def install
     system "cmake", "-S", ".", "-B", "build",
+      "-DCMAKE_CXX_FLAGS=-DLIBYUV_DISABLE_SVE -DLIBYUV_DISABLE_SME -mno-i8mm -mno-dotprod",
+      "-DCMAKE_C_FLAGS=-DLIBYUV_DISABLE_SVE -DLIBYUV_DISABLE_SME -mno-i8mm -mno-dotprod",
       "-DCMAKE_OSX_ARCHITECTURES=arm64",
       *std_cmake_args
     system "cmake", "--build", "build"
